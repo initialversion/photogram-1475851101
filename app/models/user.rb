ab352747e20d090221fac96218c37c03599eba33
Raghu Betina <raghu@firstdraft.com>
2016-10-07 14:41:04 +0000

    Direct association between users and photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :foreign_key => "owner_id",
+             :dependent => :destroy
+
   has_many   :comments,
              :foreign_key => "commenter_id",
              :dependent => :destroy
